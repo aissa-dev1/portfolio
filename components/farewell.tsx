0e48278ad448409febcc8143de01fa21f7247999
aissa-dev-1 <aissabedr74@gmail.com>
2024-07-07 16:54:04 +0100

    Remove my picture and live gradient text

--- components/farewell.tsx
+++ components/farewell.tsx
@@ -8,8 +8,9 @@ export default function Farewell() {
       className="flex flex-col items-center justify-center text-center gap-4"
     >
       <h3 className="text-5xl font-bold capitalize">
-        Ready to take your <LiveGradientText text="Web Application" /> to the
-        Next level?
+        Ready to take your{" "}
+        <span className="text-purple-700">web application</span> to the Next
+        level?
       </h3>
       <p>Reach me out today, I can help you begin, continue, complete.</p>
       <ContactMeDialog />
--- components/welcome.tsx
+++ components/welcome.tsx
@@ -1,20 +1,12 @@
-import Image from "next/image";
 import ArrowTopRightIcon from "./icons/arrowTopRight";
 import { Button } from "./ui/button";
-import LiveGradientText from "./liveGradientText";
 
 export default function Welcome() {
   return (
     <div className="flex flex-col items-center justify-center text-center gap-4">
-      <Image
-        src="/me.jpg"
-        alt="me"
-        width={200}
-        height={200}
-        className="h-[200px] rounded-full"
-      />
       <h3 className="text-5xl font-bold capitalize">
-        Transforming ideas into <LiveGradientText text="online realities" />
+        Transforming ideas into{" "}
+        <span className="text-purple-700">online realities</span>
       </h3>
 
       <p>Hi I&apos;m Aissa Bedr, a Full Stack Web Developer from Algeria.</p>
