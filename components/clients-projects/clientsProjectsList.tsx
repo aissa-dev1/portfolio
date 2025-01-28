cbc2db2010b11ad29241beb72dad6d56d5ea3dbf
aissa-dev1 <aissadev1@gmail.com>
2025-01-28 18:51:50 +0100

    use pagination for both normal and clients projects

--- components/clients-projects/clientsProjectsList.tsx
+++ components/clients-projects/clientsProjectsList.tsx
@@ -1,22 +1,49 @@
+"use client";
+
+import { usePagination } from "@/hooks/use-pagination";
 import ClientFullStackProjectCard from "./clientFullStackProjectCard";
 import ClientProjectCard, { ClientProjectType } from "./clientProjectCard";
+import { Button } from "../ui/button";
+import { scrollToElement } from "@/utils/scroll-to-element";
 
 interface Props {
   list: ClientProjectType[];
 }
 
 export default function ClientsProjectsList({ list }: Props) {
-  return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-      {list.map((project) => {
-        switch (project.type) {
-          case "full":
-            return <ClientFullStackProjectCard key={project.id} {...project} />;
+  const { page, setPage, pages, getPaginatedList } = usePagination(list, 2);
 
-          default:
-            return <ClientProjectCard key={project.id} {...project} />;
-        }
-      })}
-    </div>
+  return (
+    <>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        {getPaginatedList().map((project) => {
+          switch (project.type) {
+            case "full":
+              return (
+                <ClientFullStackProjectCard key={project.id} {...project} />
+              );
+
+            default:
+              return <ClientProjectCard key={project.id} {...project} />;
+          }
+        })}
+      </div>
+      <div className="w-full flex justify-between sm:w-1/2 sm:justify-evenly sm:mx-auto">
+        {Array.from({ length: pages }).map((_, i) => (
+          <Button
+            key={i}
+            onClick={() => {
+              if (page !== i + 1) {
+                setPage(i + 1);
+                scrollToElement("clients_projects");
+              }
+            }}
+            variant={page === i + 1 ? "default" : "outline"}
+          >
+            {i + 1}
+          </Button>
+        ))}
+      </div>
+    </>
   );
 }
--- components/projects/projectsList.tsx
+++ components/projects/projectsList.tsx
@@ -1,22 +1,47 @@
+"use client";
+
+import { scrollToElement } from "@/utils/scroll-to-element";
+import { Button } from "../ui/button";
 import FullStackProjectCard from "./fullStackProjectCard";
 import ProjectCard, { ProjectType } from "./projectCard";
+import { usePagination } from "@/hooks/use-pagination";
 
 interface Props {
   list: ProjectType[];
 }
 
 export default function ProjectsList({ list }: Props) {
-  return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-      {list.map((project) => {
-        switch (project.type) {
-          case "full":
-            return <FullStackProjectCard key={project.id} {...project} />;
+  const { page, setPage, pages, getPaginatedList } = usePagination(list, 4);
 
-          default:
-            return <ProjectCard key={project.id} {...project} />;
-        }
-      })}
-    </div>
+  return (
+    <>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        {getPaginatedList().map((project, i) => {
+          switch (project.type) {
+            case "full":
+              return <FullStackProjectCard key={project.id} {...project} />;
+
+            default:
+              return <ProjectCard key={project.id} {...project} />;
+          }
+        })}
+      </div>
+      <div className="w-full flex justify-between sm:w-1/2 sm:justify-evenly sm:mx-auto">
+        {Array.from({ length: pages }).map((_, i) => (
+          <Button
+            key={i}
+            onClick={() => {
+              if (page !== i + 1) {
+                setPage(i + 1);
+                scrollToElement("projects");
+              }
+            }}
+            variant={page === i + 1 ? "default" : "outline"}
+          >
+            {i + 1}
+          </Button>
+        ))}
+      </div>
+    </>
   );
 }
