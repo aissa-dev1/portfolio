e86c632bd892bfac0cb31a368cb413f1e5603309
aissa-dev1 <aissadev1@gmail.com>
2025-02-27 22:27:21 +0100

    update type name and import update skill item props by replacing icon object with single icon and dynamicIcon

--- data/skills.ts
+++ data/skills.ts
@@ -1,85 +1,101 @@
-import { SkillType } from "@/components/about/skill";
+import { SkillProps } from "@/components/skill";
 import { random } from "@/utils/random";
 
-export const skills: SkillType[] = [
+export const skills: SkillProps[] = [
   {
     id: random.id(),
-    icon: { current: "html", dynamicIcon: false },
+    icon: "html",
+    dynamicIcon: false,
     body: "HTML",
   },
   {
     id: random.id(),
-    icon: { current: "css", dynamicIcon: false },
+    icon: "css",
+    dynamicIcon: false,
     body: "CSS",
   },
   {
     id: random.id(),
-    icon: { current: "scss", dynamicIcon: false },
+    icon: "scss",
+    dynamicIcon: false,
     body: "Scss",
   },
   {
     id: random.id(),
-    icon: { current: "reactjs", dynamicIcon: false },
+    icon: "reactjs",
+    dynamicIcon: false,
     body: "React js",
   },
   {
     id: random.id(),
-    icon: { current: "tailwindcss", dynamicIcon: false },
+    icon: "tailwindcss",
+    dynamicIcon: false,
     body: "Tailwind css",
   },
   {
     id: random.id(),
-    icon: { current: "redux", dynamicIcon: false },
+    icon: "redux",
+    dynamicIcon: false,
     body: "Redux",
   },
   {
     id: random.id(),
-    icon: { current: "nextjs", dynamicIcon: true },
+    icon: "nextjs",
+    dynamicIcon: true,
     body: "Next js",
   },
   {
     id: random.id(),
-    icon: { current: "redux", dynamicIcon: false },
+    icon: "redux",
+    dynamicIcon: false,
     body: "Redux Toolkit",
   },
   {
     id: random.id(),
-    icon: { current: "solid", dynamicIcon: false },
+    icon: "solid",
+    dynamicIcon: false,
     body: "Solid js",
   },
   {
     id: random.id(),
-    icon: { current: "zustand", dynamicIcon: false },
+    icon: "zustand",
+    dynamicIcon: false,
     body: "Zustand",
   },
   {
     id: random.id(),
-    icon: { current: "nodejs", dynamicIcon: false },
+    icon: "nodejs",
+    dynamicIcon: false,
     body: "Node js",
   },
   {
     id: random.id(),
-    icon: { current: "expressjs", dynamicIcon: true },
+    icon: "expressjs",
+    dynamicIcon: true,
     body: "Express js",
   },
   {
     id: random.id(),
-    icon: { current: "nestjs", dynamicIcon: false },
+    icon: "nestjs",
+    dynamicIcon: false,
     body: "Nest js",
   },
   {
     id: random.id(),
-    icon: { current: "deno", dynamicIcon: false },
+    icon: "deno",
+    dynamicIcon: false,
     body: "Deno js",
   },
   {
     id: random.id(),
-    icon: { current: "git", dynamicIcon: false },
+    icon: "git",
+    dynamicIcon: false,
     body: "Git",
   },
   {
     id: random.id(),
-    icon: { current: "vitejs", dynamicIcon: false },
+    icon: "vitejs",
+    dynamicIcon: false,
     body: "Vite",
   },
 ];
