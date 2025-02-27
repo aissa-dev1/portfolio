1cbebc232eb8971ff4ec0b6ac269d03ec187a513
aissa-dev1 <aissadev1@gmail.com>
2025-02-27 22:55:04 +0100

    renaming FareWell component to ContactMe and moving it to components folder inside app

--- app/components/contact-me/index.tsx
+++ app/components/contact-me/index.tsx
@@ -1,17 +1,14 @@
-import ContactMeDialog from "./contactMeDialog";
+import ContactMeDialog from "./contact-me-dialog";
 
-export default function Farewell() {
+export default function ContactMe() {
   return (
-    <div
-      id="contact"
-      className="flex flex-col items-center justify-center text-center gap-4"
-    >
+    <section id="contact" className="flex__col flex__center text-center gap-4">
       <h3 className="text-2xl font-bold capitalize">
         Ready to take your <span className="text-color">web application</span>{" "}
         to the Next level?
       </h3>
       <p>Reach me out today, I can help you begin, continue, complete.</p>
       <ContactMeDialog />
-    </div>
+    </section>
   );
 }
