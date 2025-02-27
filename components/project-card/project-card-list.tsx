baa13c6b7fb99f3865c3eb06a19f5938f7d0411e
aissa-dev1 <aissadev1@gmail.com>
2025-02-27 22:40:56 +0100

    create ProjectCardList

--- components/project-card/project-card-list.tsx
+++ components/project-card/project-card-list.tsx
@@ -1,32 +1,32 @@
 "use client";
 
 import { usePagination } from "@/hooks/use-pagination";
-import ClientFullStackProjectCard from "./clientFullStackProjectCard";
-import ClientProjectCard, { ClientProjectType } from "./clientProjectCard";
 import { Button } from "../ui/button";
 import { scrollToElement } from "@/utils/scroll-to-element";
+import { ProjectCard, ProjectCardProps } from ".";
 
-interface Props {
-  list: ClientProjectType[];
+interface ProjectCardListProps {
+  list: ProjectCardProps[];
+  scrollToId: string;
+  itemsPerPage: number;
 }
 
-export default function ClientsProjectsList({ list }: Props) {
-  const { page, setPage, pages, getPaginatedList } = usePagination(list, 2);
+export default function ProjectCardList({
+  list,
+  scrollToId,
+  itemsPerPage,
+}: ProjectCardListProps) {
+  const { page, setPage, pages, getPaginatedList } = usePagination(
+    list,
+    itemsPerPage
+  );
 
   return (
     <>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-        {getPaginatedList().map((project) => {
-          switch (project.type) {
-            case "full":
-              return (
-                <ClientFullStackProjectCard key={project.id} {...project} />
-              );
-
-            default:
-              return <ClientProjectCard key={project.id} {...project} />;
-          }
-        })}
+        {getPaginatedList().map((project) => (
+          <ProjectCard key={project.id} {...project} />
+        ))}
       </div>
       <div className="w-full flex justify-between sm:w-1/2 sm:justify-evenly sm:mx-auto">
         {Array.from({ length: pages }).map((_, i) => (
@@ -35,7 +35,7 @@ export default function ClientsProjectsList({ list }: Props) {
             onClick={() => {
               if (page !== i + 1) {
                 setPage(i + 1);
-                scrollToElement("clients_projects");
+                scrollToElement(scrollToId);
               }
             }}
             variant={page === i + 1 ? "default" : "outline"}
