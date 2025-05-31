3bf8b25db040b7f9796352ac3def2ea9cf3b22c9
aissa-dev1 <aissadev1@gmail.com>
2025-05-31 17:00:12 +0100

    Remove default svgs and add logo and add home info and style button and link elms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 import styles from "./page.module.css";
 
 import { Container } from "@/components/Container";
@@ -6,14 +9,24 @@ export default function Home() {
   return (
     <main>
       <Container>
-        <h1>Hi Aissa</h1>
-        <h2>Hello world!</h2>
+        <Image
+          src="/logo.png"
+          alt="Aissa Bedr Logo"
+          className={styles.logo}
+          width={200}
+          height={200}
+        />
+        <h1>Hey, It's me Aissa Bedr</h1>
+        <h2 className={styles.h2}>Full Stack Developer</h2>
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          Necessitatibus dolorem illum qui obcaecati molestiae minima aperiam,
-          impedit dolorum! Incidunt consequuntur sapiente necessitatibus
-          distinctio reiciendis blanditiis optio unde debitis nulla numquam?
+          A self-taught developer with 3+ years of experience in web
+          development, specializing in TypeScript, Next.js, and Nest.js. I enjoy
+          building fast, scalable, and reusable components, whether for the
+          frontend or backend.
         </p>
+        <Link href="/contact">
+          <button>Reach out</button>
+        </Link>
       </Container>
     </main>
   );
